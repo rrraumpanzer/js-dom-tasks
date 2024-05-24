eafc55e8cd757cc6e3d50aa01de81171d951b99b
rrraumpanzer <ayylmao3713@gmail.com>
2024-05-24 19:09:00 +0300

    1 2 3?

--- solutions/2-bom.js
+++ solutions/2-bom.js
@@ -1,3 +1,8 @@
 // BEGIN
-
+const traverse = (link) => {
+    let browserInfo = navigator.userAgent.split(" ")[0] + ' ' + link;
+    window.location.href = link;
+    return browserInfo;
+}
+export default traverse;
 // END
--- solutions/3-dom.js
+++ solutions/3-dom.js
@@ -1,3 +1,12 @@
 // BEGIN
+body = document.body.innerHTML;
+const lines = body.split("\n");
+let newBody = "";
+console.log("Hello?")
+for (var i = 0; i<length(lines); i++) {
+    newBody += "<p>" + lines[i].trim() + "</p>\n";
+}
+document.body.innerHTML = newBody;
 
+// http://127.0.0.1:8080/public/3-index.html
 // END
